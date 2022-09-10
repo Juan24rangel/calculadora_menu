08a41a3226e83b0c1fb7f30ec90d850779cf6732
Juan24rangel <juan2224658@correo.uis.edu.co>
2022-09-10 08:24:29 -0500

    codigo terminado

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -19,4 +19,44 @@ print("2. restar (el primero menos el segundo)")
 print("3. multiplicar (el primero por el segundo)")
 print("4. dividir (el primero entre el segundo)")
 print("5. potencia (el primero a la potencia del segundo)")
-print("6. log (el primero mas el segundo)")
+print("6. logaritmo (el logaritmo del primero)")
+
+opcion = int(input("\nDame la opcion: "))
+
+# processing
+
+if(opcion==1):
+    z=x+y
+    print(x,"+",y)
+elif(opcion==2):
+    z=x-y
+    print(x,"-",y)
+elif(opcion==3):
+    z=x*y
+    print(x,"*",y)
+elif(opcion==4 and y!=0):
+    z=x/y
+    print(x,"/",y)
+elif(opcion==4 and y==0):
+    print("El denominador es igual a cero y")
+    print("NO se puede realizar la division.")
+    bandera = True
+elif(opcion==5):
+    z=pow(x,y)
+    print(x,"^",y)
+elif (opcion==6 and x>0):
+    z = log(x)
+    print("Logaritmo de", x)
+elif(opcion==6 and x<=0):
+    print("el valor de x es menor o igaul a cero y")
+    print("NO se puede calcular el logaritmo.")
+    bandera = True
+else:
+    print("Opcion no valida")
+    
+# se escribe el resultado con otra condicion
+
+if (opcion<7 and bandera==False):
+    print("Resultado:", z)
+    
+#fin
