49e3e8a2c67feaeda992797556f3dbd3c26ebac1
Juan24rangel <juan2224658@correo.uis.edu.co>
2022-09-10 07:51:00 -0500

    se agrego input en codigo python

---
+++ calculadora_menu.py
@@ -0,0 +1,22 @@
+# calculadora con menu
+
+from math import log
+
+print("---------------------------")
+print("----CALCULADORA - MENU-----")
+print("---------------------------")
+
+#input
+bandera = False
+x = float(input("Digite el valor de x: "))
+y = float(input("Digite el valor de y: "))
+
+print("\nDame la opcion de deseas realizar: \n")
+
+# Se despliega el menu para seleccionar la opcion que deseas realizar
+print("1. sumar (el primero mas el segundo)")
+print("2. restar (el primero menos el segundo)")
+print("3. multiplicar (el primero por el segundo)")
+print("4. dividir (el primero entre el segundo)")
+print("5. potencia (el primero a la potencia del segundo)")
+print("6. log (el primero mas el segundo)")
